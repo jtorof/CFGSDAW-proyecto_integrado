0259efa930b39709b4ddd6b93bbc84aa00622c3f
jtorof <78854533+jtorof@users.noreply.github.com>
2022-04-10 14:00:24 +0200

    Webpack from symfony and cleanup MainController User and ApiToken. json login Several MDB things

---
+++ src/Entity/ApiToken.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ApiTokenRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ApiTokenRepository::class)]
+class ApiToken
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $token;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isEnabled;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'apiTokens')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $owner;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(string $token): self
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    public function getIsEnabled(): ?bool
+    {
+        return $this->isEnabled;
+    }
+
+    public function setIsEnabled(bool $isEnabled): self
+    {
+        $this->isEnabled = $isEnabled;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): self
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+}
