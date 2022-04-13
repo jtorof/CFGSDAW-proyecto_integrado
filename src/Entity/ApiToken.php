53850702743fc11386ab4110ff30d71232a341eb
jtorof <78854533+jtorof@users.noreply.github.com>
2022-04-13 15:15:07 +0200

    Unique tokens

--- src/Entity/ApiToken.php
+++ src/Entity/ApiToken.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ApiTokenRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ApiTokenRepository::class)]
 class ApiToken
@@ -13,7 +14,7 @@ class ApiToken
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
     private $token;
 
     #[ORM\Column(type: 'boolean')]
