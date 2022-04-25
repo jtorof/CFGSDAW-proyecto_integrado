b1a4703c86097400add6fc8962b88ccd43c20b90
jtorof <78854533+jtorof@users.noreply.github.com>
2022-04-25 23:35:42 +0200

    helmet

--- assets/routes/Login.jsx
+++ assets/routes/Login.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 import LoginForm from '../components/LoginForm';
 
 const Login = () => {
   return (
-    <>      
+    <>
+      <Helmet>
+        <title>APIParaPracticar | Iniciar Sesión</title>
+      </Helmet>
       <LoginForm />
     </>
   )
--- assets/routes/SignUp.jsx
+++ assets/routes/SignUp.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 import SignUpForm from '../components/SignUpForm';
 
 const SignUp = () => {
   return (
     <>
+      <Helmet>
+        <title>APIParaPracticar | Crear cuenta</title>
+      </Helmet>
       <SignUpForm />
     </>
   )
