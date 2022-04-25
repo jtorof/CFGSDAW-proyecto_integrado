0451bec24e6d54d4c688e88cb9c4ee17ad2507d7
jtorof <78854533+jtorof@users.noreply.github.com>
2022-04-26 00:36:42 +0200

    Added route

--- assets/helpers/routes.js
+++ assets/helpers/routes.js
@@ -5,6 +5,7 @@ routesNamesMap.set("", "Home");
 routesNamesMap.set("docs", "Documentación");
 routesNamesMap.set("login", "Iniciar Sesión");
 routesNamesMap.set("signup", "Crear Cuenta");
+routesNamesMap.set("profile", "Perfil de Usuario");
 routesNamesMap.set("admin", "Administración");
 routesNamesMap.set(regExp, "test");
 
