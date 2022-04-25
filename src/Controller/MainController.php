99e3e55cd95f1dfbdcc6ee5cb8a529378bfde675
jtorof <78854533+jtorof@users.noreply.github.com>
2022-04-26 00:36:25 +0200

    Basic register validation

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,7 +13,7 @@ class MainController extends AbstractController
     public function index(SerializerInterface $serializer): Response
     {
         return $this->render('main/index.html.twig', [
-            'user' => $serializer->serialize($this->getUser(), 'json'),
+            'user' => $serializer->serialize($this->getUser(), 'json', ['groups' => 'userInfo']),
         ]);
     }
 }
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,12 +9,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, ValidatorInterface $validator, EntityManagerInterface $entityManager): Response
     {
+        $error = false;
+        $message = false;
+
         try {
             $data = json_decode($request->getContent(), true);
             //file_put_contents('SOMELOG.LOG', print_r($data, true).PHP_EOL, FILE_APPEND);
@@ -30,17 +34,28 @@ class RegistrationController extends AbstractController
                     )
                 );
 
-            // Commented cause testing things
-            // $entityManager->persist($user);
-            // $entityManager->flush();
+            $errors = $validator->validate($user);
+            file_put_contents('SOMELOG.LOG', print_r($errors, true).PHP_EOL, FILE_APPEND);
+            if (count($errors) > 0) {
+                $error = (string) $errors;
+                //$error = 'Validation error';
+                file_put_contents('SOMELOG.LOG', "Entra en if".PHP_EOL, FILE_APPEND);
+            } else {
+                // Commented cause testing things
+                // $entityManager->persist($user);
+                // $entityManager->flush();
+    
+                $message = 'Account successfully created';
+            }
 
-            $message = 'Account successfully created';
         } catch (\Exception $e) {
-            $message = $e->getMessage();
+            file_put_contents('SOMELOG.LOG', "Catch".PHP_EOL, FILE_APPEND);
+            $error = $e->getMessage();
         }
         
         return $this->json([
             'message' => $message,
+            'error' => $error,
         ]);
     }
 }
