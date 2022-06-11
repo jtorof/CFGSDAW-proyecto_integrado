be80506d17ef118a1369d8270e1b907f4f94810f
jtorof <78854533+jtorof@users.noreply.github.com>
2022-06-11 21:25:20 +0200

    Improved mobile design

--- assets/components/Navigation.jsx
+++ assets/components/Navigation.jsx
@@ -41,7 +41,7 @@ const Navigation = ({ showBreadcrumbs }) => {
           {"email" in context.globalUser ?
             <MDBNavbarNav fullWidth={false}>
               <MDBNavbarItem className='navbar-text'>
-                Sesión iniciada como: {context.globalUser.email}
+                <span className='d-none d-md-inline-block'>Sesión iniciada como: </span><span></span>{context.globalUser.email}
               </MDBNavbarItem>
             </MDBNavbarNav> :
             null
